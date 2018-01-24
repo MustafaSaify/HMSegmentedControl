4f269eb31849a3e0d69ca408650a336dd7da14c2
Mustafa Saify <mustafa.saify@workpulse.com>
2018-01-24 15:00:29 +0530

    Added a setter for sectionSelectedBackgroundImage

--- HMSegmentedControl/HMSegmentedControl.m
+++ HMSegmentedControl/HMSegmentedControl.m
@@ -157,7 +157,6 @@
     self.selectionIndicatorBoxLayer = [CALayer layer];
     self.selectionIndicatorBoxLayer.opacity = self.selectionIndicatorBoxOpacity;
     self.selectionIndicatorBoxLayer.borderWidth = 1.0f;
-    self.selectionIndicatorBoxLayer.contents = (__bridge id _Nullable)(self.sectionSelectedBackgroundImage.CGImage);
     self.selectionIndicatorBoxOpacity = 0.2;
     
     self.contentMode = UIViewContentModeRedraw;
@@ -201,6 +200,12 @@
     _selectionIndicatorBoxOpacity = selectionIndicatorBoxOpacity;
     
     self.selectionIndicatorBoxLayer.opacity = _selectionIndicatorBoxOpacity;
+}
+
+- (void)setSectionSelectedBackgroundImage:(UIImage *)sectionSelectedBackgroundImage {
+    _sectionSelectedBackgroundImage = sectionSelectedBackgroundImage;
+    
+    self.selectionIndicatorBoxLayer.contents = (__bridge id _Nullable)(self.sectionSelectedBackgroundImage.CGImage);
 }
 
 - (void)setSegmentWidthStyle:(HMSegmentedControlSegmentWidthStyle)segmentWidthStyle {
